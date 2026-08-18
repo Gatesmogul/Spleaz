093fc6445803b93954f986879ac54ef467fb9a3b
Gatesmogul <ogunmola83@gmail.com>
2026-08-18 17:45:13 +0100

    Correct allowed origin URL casing

--- backend/config/socket.js
+++ backend/config/socket.js
@@ -5,7 +5,7 @@ let io;
 
 const initSocket = (httpServer) => {
   const allowedOrigins = [
-    'https://spleaz-app.onrender.com',
+    'https://Spleaz-app.onrender.com',
     'http://localhost:8081',
     'http://localhost:19006',
   ];
